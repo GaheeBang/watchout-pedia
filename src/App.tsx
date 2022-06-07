ef9a05346bbbae44e6fd11cfc98d1e71ee3ca529
GaheeBang <gladhees2@gmail.com>
2022-06-07 15:01:07 +0900

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import MainPage from './pages/MainPage';
+import MovieDetail from './pages/MovieDetail';
+import TvPage from './pages/TvPage';
+import TvDetail from './pages/TvDetail';
+
+import LoginModal from './features/app/LoginModal';
+import SignupModal from './features/app/SignupModal';
+
+const Base = styled.div``;
+
+function App() {
+  return (
+    <Base>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/tv" element={<TvPage />} />
+          <Route path="/movie/:id" element={<MovieDetail />} />
+          <Route path="/tv/:id" element={<TvDetail />} />
+        </Routes>
+      </Router>
+      <LoginModal />
+      <SignupModal />
+    </Base>
+  );
+}
+
+export default App;
